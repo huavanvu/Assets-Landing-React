7568942ee5ae125569dd37b40895a6c142daca09
hieusmiths <hieusmiths@gmail.com>
2019-09-23 15:22:16 +0700

    update about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -34,62 +34,86 @@ export default class AboutPage extends Component {
             <img src="../../resources/gioithieu/Rectangle 4.png" />
           </div>
           <div className="text-justify">
-          Theo nhận định của các chuyên gia, Việt Nam là một thị trường mới nổi năng
-          động bậc nhất toàn cầu, với hai yếu tố nổi bật là tốc độ tăng trưởng kinh tế mạnh
-          mẽ và sự gia tăng thu nhập của tầng lớp trung lưu ngày một lớn mạnh. Cùng
-          với đó là những lợi thế về dân số trẻ có học thức cao đã giúp quá trình đô thị hóa
-          diễn ra nhanh chóng. Điều này giúp thúc đẩy triển vọng của các nhà đầu tư quốc
-          tế trong việc tạo dấu ấn tại thị trường bất động sản có nhiều tiềm năng như Việt
-          Nam.
+          Theo nhận định của các chuyên gia, Việt Nam là một trong những thị trường mới
+          nổi năng động nhất toàn cầu, với hai yếu tố nổi bật là tốc độ tăng trưởng kinh tế
+          mạnh mẽ và sự gia tăng thu nhập của tầng lớp trung lưu. Cùng với đó là những lợi
+          thế về dân số trẻ có học thức cao đã thúc đẩy quá quá trình đô thị hóa diễn ra một
+          cách nhanh chóng. Những yếu tố này đã đưa Việt Nam thành một trong những
+          quốc gia có tốc độ tăng trưởng GDP nhanh nhất thế giới và đó cũng là lý do thúc
+          đẩy triển vọng của các nhà đầu tư quốc tế trong việc nhanh chóng tạo dấu ấn tại
+          <strong> thị trường bất động sản </strong> vô cùng tiềm năng như Việt Nam.
           <br /> <br />
           Bên cạnh một thị trường bất động sản đầy tiềm năng thì môi giới bất động sản trở
-          thành loại hình dịch vụ chính thức trên thị trường bất động sản, là cầu nối giữa
-          nhà đầu tư, chủ dự án với người có nhu cầu giao dịch bất động sản. Thời gian qua,
-          theo đánh giá, lực lượng này trở thành một phần quan trọng, là nhân tố tạo thanh
-          khoản cho thị trường bất động sản.
-          <br /> <br />
-          Tuy nhiên trong một kỷ nguyên công nghệ số 4.0 như hiện nay, việc kiểm soát,
-          quản lý, cũng như đảm bảo các thông tin pháp lý của bất động sản thật sự là một
-          bài toán khó đối với thị trường bất động sản Việt Nam.
-          Hiện tại, ở Việt Nam lĩnh vực bất động sản vẫn chưa thỏa mãn được điều kiện để
-          được xem là thị trường, bởi các thông tin quá hạn chế, không rõ ràng, muốn biết
-          thông tin phải sử dụng các kênh khác nhau, chưa kể đến việc thông tin thu thập
-          được lại không chính xác, nhiều thông tin gây nhiễu.
-          <br /> <br />
-          Khi có nhu cầu tìm hiểu cụ thể về bất động sản, các yếu tố được quan tâm là gì:
-          thông tin quy hoạch, định giá bất động sản, thông tin pháp lý, thông tin các chủ
-          đầu tư, chủ dự án bất động sản, lịch sử giá bất động sản, lịch sử giao dịch mua,
-          bán, cho thuê bất động sản... hay là những phân tích, đánh giá của các chuyên
-          gia hàng đầu trong lĩnh vực bất động sản? Tất cả những những thông tin trên đều
-          cần thiết, vậy làm sao những thông tin trên có thể đến được với tất cả mọi người,
-          mọi thành phần, mọi tầng lớp trong xã hội, và trên hết là đơn giản hóa các bước
-          chỉ trong 1 phạm vi tìm kiếm.
-          <br /> <br />
-          Làm sao để đảm bảo quyền lợi của cả chủ đầu tư, chủ dự án, người mua, người
-          bán,... mà không bị một bên thứ ba nào can thiệp và trục lợi bất chính từ các giao
-          dịch thành công.
-          </div>
+          thành loại hình dịch vụ chính thức trên thị trường bất động sản, là cầu nối quan
+          trọng tạo tiền đề cho cung - cầu bất động sản gặp được nhau thông qua những
+          thông tin thu thập được để giải quyết vấn đề liên quan đến bất động sản. Chính
+          vì vậy, lực lượng này trở thành một phần quan trọng, là nhân tố tạo thanh khoản
+          cho thị trường bất động sản và là nhân tố đảm bảo sự phát triển của thị trường
+          bất động sản theo hướng bền vững, hiệu quả.
           <div className="text-center mt-5 mb-5">
-            <img  src="../../resources/gioithieu/Bảo vệ quyền lợi tối đa của tất cả các bên.png" />
+            <img src="../../resources/gioithieu/top-dbs.png" />
+          </div>
+          <br /> <br />
+          Lâu nay, thị trường bất động sản được coi là một trong những đầu kéo chủ lực
+          của nền kinh tế Việt Nam. Tuy nhiên, do các chính sách pháp lý chưa thực sự
+          hoàn chỉnh và sự thiếu minh bạch trong thông tin khiến bất động sản, bên cạnh
+          là thị trường đầu tư hiệu quả thì vẫn luôn được xem là thị trường tiềm ẩn nhiều rủi
+          ro. Hiện tại, ở Việt Nam lĩnh vực bất động sản vẫn chưa thỏa mãn được điều kiện
+          để được xem là thị trường, bởi sự hạn chế và thiếu minh bạch thông tin về pháp
+          lý, quy hoạch, giá cả, diện tích, quy mô bất động sản, thủ tục giấy tờ... Các thông
+          tin liên quan đến bất động sản đươc cung cấp một cách nhỏ giọt, rời rạc, thiếu
+          chính xác. Người tiêu dùng khi muốn biết thông tin phải tìm hiểu qua rất nhiều
+          kênh thông tin khác nhau, gây nên sự lãng phí về thời gian và chi phí, nhưng
+          không mang lại tính hiệu quả cao. Điều này, vô hình trung tạo ra các bất cập trong
+          việc giao dịch bất động sản, đồng thời tạo ra lỗ hổng pháp lý dẫn tới việc trục lợi
+          của một số đối tượng gây thiệt hại to lớn cho người sử dụng.
+          <br /> <br />
+          Chính vì vậy, bài toán kiểm soát, quản lý, cũng như đảm bảo tính chính xác của
+          các thông tin pháp lý của mỗi bất động sản nói riêng và thị trường bất động sản
+          nói chung trong một kỷ nguyên công nghệ số 4.0 như hiện nay lại càng trở nên
+          nan giải hơn bao giờ hết.
+          <div className="text-center mt-5 mb-5">
+            <img  src="../../resources/gioithieu/bao-ve-quyen-loi-doi-ben.png" />
+          </div>
+          <br /> <br />
+          <span className="asset-name"> ASSET</span> ra đời với mục đích đơn giản hóa và gói gọn tất cả các thông tin về bất
+          động sản minh bạch nhất và đầy đủ chỉ trong 1 phạm vi tìm kiếm. <span className="asset-name"> ASSET</span> tạo hệ
+          sinh thái bất động sản sạch, với tuyên ngôn bảo vệ quyền lợi tối đa của tất cả các
+          bên, từ chủ đầu tư, chủ dự án, người mua, người bán, môi giới.. xuyên suốt quá
+          trình giao dịch bất động sản mà không bị tác động tiêu cực và can thiệp từ bất kì
+          bên thứ ba nào.
+          <br /> 
+          Với định hướng xây dựng là một GOOGLE về bất động sản của người dân Việt
+          Nam, <span className="asset-name"> ASSET</span> tạo ra một thị trường bất động sản online thu nhỏ nơi mà, tại đây,
+          người dùng có thể tra cứu, tìm hiểu mọi thông tin liên quan đến bất động sản một
+          cách dễ dàng, từ thông tin quy hoạch, định giá bất động sản, thông tin pháp lý,
+          thông tin các chủ đầu tư, chủ dự án bất động sản, lịch sử giá bất động sản, lịch sử
+          giao dịch mua, bán, cho thuê bất động sản, cho đến những phân tích, đánh giá
+          của các chuyên gia hàng đầu trong lĩnh vực bất động sản. Tất cả sẽ được gói gọn
+          trong một cú click chuột, vô cùng thuận tiện và đặc biệt người sử dụng sẽ không
+          mất bất kỳ một chi phí nào. Tất cả các thông tin từ trang GOOGLE bất động sản
+          <span className="asset-name"> ASSET</span> sẽ được cập nhật liên tục tương ứng với sự phát triển của thị trường. Tất
+          cả các thông tin trên <span className="asset-name"> ASSET</span> đều được các chuyên gia đầu ngành, các luật sư
+          <div className="text-center mt-5 mb-5">
+            <img  src="../../resources/gioithieu/map.png" />
           </div>
           <div className="text-justify">
-            <span className="asset-name text-j">ASSET</span> ra đời với tuyên ngôn minh bạch và bảo vệ quyền lợi tối đa của tất cả các
-            bên, tạo hệ sinh thái bất động sản sạch. Như một thị trường bất động sản online
-            thu nhỏ, <span className="asset-name">ASSET</span> chủ trương sẽ là một GOOGLE về bất động sản của người dân Việt
-            Nam. Tại đây, người dùng có thể tra cứu, tìm hiểu mọi thông tin liên quan đến bất
-            động sản một cách dễ dàng, thuận tiện mà không mất bất kỳ một chi phí nào. Tất
-            cả các thông tin từ trang GOOGLE bất động sản <span className="asset-name">ASSET</span> đều được các chuyên gia
-            đầu ngành, các luật sư kiểm chứng và đảm bảo.
-            <br /> <br />
-            Không chỉ để tra cứu các thông tin bất động sản, <span className="asset-name">ASSET</span> còn là một cầu nối uy tín
-            để người dùng kết nối các nhu cầu đầu tư, mua, bán hoặc cho thuê bất động sản
-            đến khách hàng mục tiêu một cách nhanh chóng và an toàn tuyệt đối một cách
-            tiết kiệm nhất. Các chuyên gia hàng đầu của <span className="asset-name">ASSET</span> sẽ kiểm chứng, sàng lọc tất
-            cả các thông tin giữa các bên và kết nối một cách minh bạch công khai các bên
-            với nhau mà không tính bất kỳ một khoản chi phí phát sinh nào.
+          Không chỉ để tra cứu các thông tin bất động sản, <span className="asset-name"> ASSET</span> còn là một cầu nối uy tín
+          để người dùng kết nối các nhu cầu đầu tư, mua, bán hoặc cho thuê bất động sản
+          đến khách hàng mục tiêu một cách nhanh chóng và an toàn tuyệt đối một cách
+          tiết kiệm nhất. Các chuyên gia hàng đầu của <span className="asset-name"> ASSET</span> sẽ kiểm chứng, sàng lọc tất
+          cả các thông tin giữa các bên và kết nối một cách minh bạch công khai các bên
+          với nhau mà không tính bất kỳ một khoản phí phát sinh nào.
+          <br /> <br />
+          Với sứ mệnh “ xây dựng một thị trường bất động sản minh bạch”, chúng tôi cam
+          kết trở thành một người bạn đồng hành không thể thiếu và đáng tin cậy trên
+          chặng đường hoạch định, xây dựng và phát triển bất động sản của mỗi người
+          dân Việt Nam, từ đó cùng nhau chung tay hướng đến thị trường bất động sản
+          sạch, ngày một lớn mạnh và phát triển bền vững.
+          </div>
           </div>
           <div className="text-center mt-5 mb-5">
-            <img src="../../resources/gioithieu/“Minh bạch thị trường bất động sản”.png" />
+            <img src="../../resources/gioithieu/minh-bach.png" />
           </div>
           <div className="text-center slogan-footer">
             Chúng tôi tin tưởng rằng sự ra đời của <span className="asset-name">ASSET</span> sẽ mở ra một kỷ nguyên mới,
